2b9c6e669d250592a7ba7eafdbe5f1e8eb0bae39
Jin mo Kim <skim112790@yahoo.com>
2022-01-04 18:59:35 -0500

    Finished activity + 1 bonus

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 class Casino {
-    // Write code here
+    constructor(name, isFakeCoin){
+        this.name = name
+        this.isFakeCoin = isFakeCoin
+        this.timesPlayed = 0;
+        this.amountWon = 0;
+    }
+    playGame(betAmount) {
+        if(this.isFakeCoin == true){
+            console.log(this.name + " wins!")
+            this.timesPlayed += 1
+        } else if(Math.random() <= 0.5) {
+            console.log(this.name + " wins!")
+            this.timesPlayed += 1
+        } else {
+            console.log("Player wins!")
+            this.timesPlayed += 1
+            this.amountWon = betAmount * (this.timesPlayed + 1)
+        }
+        
+    }
 };
 
 // TESTS
-const myCasino = new Casino("HackerU Casino");
+const myCasino = new Casino("HackerU Casino", true);
 console.log(myCasino);
 myCasino.playGame(5);
 myCasino.playGame(15);
